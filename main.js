9d59600afefcd1f74754b211c0f83ecce6f6f602
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-23 20:33:48 +0300

    step-4

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ console.log(colorBtn);
 
 const list = document.querySelector(".list")
 console.log(list);
+
+const domItem = document.querySelector("#dom")
+console.log(domItem);
+
+const items = document.querySelectorAll(".item")
+console.log(items);
+console.log([1,2,3,4]);
+
+domItem.innerHTML = '<a href="https://developer.mozilla.org/ru/docs/Web/API/Document_Object_Model/Introduction">Взаимодействие с DOM ⬅</a>'
