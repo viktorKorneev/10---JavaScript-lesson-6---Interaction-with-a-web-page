3692d0e5e24296d4728d3f965dc259a3ac4b49bf
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-26 18:24:08 +0300

    DOM

--- DOM/main.js
+++ DOM/main.js
@@ -1,19 +1,20 @@
-// function init() {
-//   let planet = document.getElementById("greenplanet");
-//   planet.innerHTML = "Red Alert: hit by phaser fire!";
-// }
-
-// window.onload = init;
-// -------------------------------------------------------
-
-function addSongs() {
-  let song1 = document.getElementById("song1");
-  let song2 = document.getElementById("song2");
-  let song3 = document.getElementById("song3");
-
-  song1.innerHTML = "Blue Suede Strings, by Elvis Pagely";
-  song2.innerHTML = "Great Objects on Fire, by Jerry JSON Lewis";
-  song3.innerHTML = "I Code the Line, by Johnny JavaScript";
+function init() {
+  let planet = document.getElementById("greenplanet");
+  planet.innerHTML = "Red Alert: hit by phaser fire!";
+  planet.setAttribute("class", "redtext")
 }
 
-window.onload = addSongs
+window.onload = init;
+// -------------------------------------------------------
+
+// function addSongs() {
+//   let song1 = document.getElementById("song1");
+//   let song2 = document.getElementById("song2");
+//   let song3 = document.getElementById("song3");
+
+//   song1.innerHTML = "Blue Suede Strings, by Elvis Pagely";
+//   song2.innerHTML = "Great Objects on Fire, by Jerry JSON Lewis";
+//   song3.innerHTML = "I Code the Line, by Johnny JavaScript";
+// }
+
+// window.onload = addSongs
