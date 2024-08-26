f9c3bffed9b3b513e09ac70d9df3703f0e3c0acd
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-26 18:11:46 +0300

    DOM

--- DOM/main.js
+++ DOM/main.js
@@ -1,6 +1,19 @@
-function init() {
-  let planet = document.getElementById("greenplanet");
-  planet.innerHTML = "Red Alert: hit by phaser fire!";
+// function init() {
+//   let planet = document.getElementById("greenplanet");
+//   planet.innerHTML = "Red Alert: hit by phaser fire!";
+// }
+
+// window.onload = init;
+// -------------------------------------------------------
+
+function addSongs() {
+  let song1 = document.getElementById("song1");
+  let song2 = document.getElementById("song2");
+  let song3 = document.getElementById("song3");
+
+  song1.innerHTML = "Blue Suede Strings, by Elvis Pagely";
+  song2.innerHTML = "Great Objects on Fire, by Jerry JSON Lewis";
+  song3.innerHTML = "I Code the Line, by Johnny JavaScript";
 }
 
-window.onload = init;
+window.onload = addSongs
